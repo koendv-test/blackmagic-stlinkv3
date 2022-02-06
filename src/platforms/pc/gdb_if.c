b433a0e21cf987b204789ffdeb072a3d772983d7
Stoyan Shopov <stoyan.shopov@gmail.com>
2022-02-06 14:41:42 +0200

    Add an empty sidekick thread to bmp-hosted.

--- src/platforms/pc/gdb_if.c
+++ src/platforms/pc/gdb_if.c
@@ -39,13 +39,165 @@
 #include "general.h"
 #include <assert.h>
 #include <errno.h>
+#include <pthread.h>
 #include <unistd.h>
 
 #include "gdb_if.h"
 
+enum
+{
+	MAX_SOCKET_RETRIES	= 4,
+	SIDEKICK_PORT		= 4000,
+	DEFAULT_PORT		= 2000,
+};
+
+static struct
+{
+pthread_t thread;
+pthread_attr_t attr;
+}
+sidekick_data;
+
+
+static int socket_accept(int listening_socket_fd)
+{
+	int socket_fd;
+#if defined(_WIN32) || defined(__CYGWIN__)
+	int iResult;
+	unsigned long opt;
+#else
+	int flags;
+#endif
+#if defined(_WIN32) || defined(__CYGWIN__)
+	opt = 1;
+	iResult = ioctlsocket(listening_socket_fd, FIONBIO, &opt);
+	if (iResult != NO_ERROR) {
+		DEBUG_WARN("ioctlsocket failed with error: %ld\n", iResult);
+	}
+#else
+	flags = fcntl(listening_socket_fd, F_GETFL);
+	fcntl(listening_socket_fd, F_SETFL, flags | O_NONBLOCK);
+#endif
+	while(1) {
+		socket_fd = accept(listening_socket_fd, NULL, NULL);
+		if (socket_fd == -1) {
+#if defined(_WIN32) || defined(__CYGWIN__)
+			if (WSAGetLastError() == WSAEWOULDBLOCK) {
+#else
+			if (errno == EWOULDBLOCK) {
+#endif
+				SET_IDLE_STATE(1);
+				platform_delay(100);
+			} else {
+#if defined(_WIN32) || defined(__CYGWIN__)
+				DEBUG_WARN("error when accepting connection: %d",
+						   WSAGetLastError());
+#else
+				DEBUG_WARN("error when accepting connection: %s",
+						   strerror(errno));
+#endif
+				exit(1);
+			}
+		} else {
+#if defined(_WIN32) || defined(__CYGWIN__)
+			opt = 0;
+			ioctlsocket(listening_socket_fd, FIONBIO, &opt);
+#else
+			fcntl(listening_socket_fd, F_SETFL, flags);
+#endif
+			break;
+		}
+	}
+	DEBUG_INFO("Got connection\n");
+#if defined(_WIN32) || defined(__CYGWIN__)
+	opt = 0;
+	ioctlsocket(socket_fd, FIONBIO, &opt);
+#else
+	flags = fcntl(socket_fd, F_GETFL);
+	fcntl(socket_fd, F_SETFL, flags & ~O_NONBLOCK);
+#endif
+	return socket_fd;
+}
+
+static int create_socket(int port, int * socket_fd)
+{
+	struct sockaddr_in addr;
+	int opt, tries = 0;
+
+	do {
+		port ++;
+		tries ++;
+		if (tries > MAX_SOCKET_RETRIES)
+			return - 1;
+		addr.sin_family = AF_INET;
+		addr.sin_port = htons(port);
+		addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+		* socket_fd = socket(PF_INET, SOCK_STREAM, 0);
+		if (* socket_fd == -1) {
+			DEBUG_WARN("PF_INET %d\n",* socket_fd);
+			continue;
+		}
+
+		opt = 1;
+		if (setsockopt(* socket_fd, SOL_SOCKET, SO_REUSEADDR, (void*)&opt, sizeof(opt)) == -1) {
+#if defined(_WIN32) || defined(__CYGWIN__)
+		    DEBUG_WARN("error setsockopt SOL_SOCKET : %d error: %d\n", * socket_fd,
+			WSAGetLastError());
+#else
+			DEBUG_WARN("error setsockopt SOL_SOCKET : %d error: %d\n", * socket_fd,
+			strerror(errno));
+#endif
+			close(* socket_fd);
+			continue;
+		}
+		if (setsockopt(* socket_fd, IPPROTO_TCP, TCP_NODELAY, (void*)&opt, sizeof(opt)) == -1) {
+#if defined(_WIN32) || defined(__CYGWIN__)
+			DEBUG_WARN("error setsockopt IPPROTO_TCP : %d error: %d\n", * socket_fd,
+			WSAGetLastError());
+#else
+			DEBUG_WARN("error setsockopt IPPROTO_TCP : %d error: %d\n", * socket_fd,
+			strerror(errno));
+#endif
+			close(* socket_fd);
+			continue;
+		}
+		if (bind(* socket_fd, (void*)&addr, sizeof(addr)) == -1) {
+#if defined(_WIN32) || defined(__CYGWIN__)
+			DEBUG_WARN("error when binding socket: %d error: %d\n", * socket_fd,
+			WSAGetLastError());
+#else
+			DEBUG_WARN("error when binding socket: %d error: %d\n", * socket_fd,
+			strerror(errno));
+#endif
+			close(* socket_fd);
+			continue;
+		}
+		if (listen(* socket_fd, 1) == -1) {
+			DEBUG_WARN("listen closed %d\n",* socket_fd);
+			close(* socket_fd);
+			continue;
+		}
+		break;
+	} while(1);
+	return port;
+}
+
+static void * sidekick(void * param)
+{
+int socket_fd;
+
+	(void) param;
+	DEBUG_WARN("sidekick listening on port %i\n", create_socket(SIDEKICK_PORT - 1, & socket_fd));
+	fflush(stderr);
+
+	DEBUG_WARN("sidekick thread bailing out\n");
+	fflush(stderr);
+	return 0;
+}
+
 static int gdb_if_serv, gdb_if_conn;
-#define DEFAULT_PORT 2000
-#define NUM_GDB_SERVER 4
+
 int gdb_if_init(void)
 {
 #if defined(_WIN32) || defined(__CYGWIN__)
@@ -57,66 +209,19 @@ int gdb_if_init(void)
 		exit(1);
 	}
 #endif
-	struct sockaddr_in addr;
-	int opt;
-	int port = DEFAULT_PORT - 1;
+	/* Deploy sidekick thread. */
+	if (pthread_attr_init(&sidekick_data.attr))
+	{
+		DEBUG_WARN("pthread_attr_init() error\n");
+		exit(1);
+	}
+	if (pthread_create(&sidekick_data.thread, &sidekick_data.attr, sidekick, 0))
+	{
+		DEBUG_WARN("pthread_create() error\n");
+		exit(1);
+	}
 
-	do {
-		port ++;
-		if (port > DEFAULT_PORT + NUM_GDB_SERVER)
-			return - 1;
-		addr.sin_family = AF_INET;
-		addr.sin_port = htons(port);
-		addr.sin_addr.s_addr = htonl(INADDR_ANY);
-
-		gdb_if_serv = socket(PF_INET, SOCK_STREAM, 0);
-		if (gdb_if_serv == -1) {
-			DEBUG_WARN("PF_INET %d\n",gdb_if_serv);
-			continue;
-		}
-
-		opt = 1;
-		if (setsockopt(gdb_if_serv, SOL_SOCKET, SO_REUSEADDR, (void*)&opt, sizeof(opt)) == -1) {
-#if defined(_WIN32) || defined(__CYGWIN__)
-		    DEBUG_WARN("error setsockopt SOL_SOCKET : %d error: %d\n", gdb_if_serv,
-			WSAGetLastError());
-#else
-			DEBUG_WARN("error setsockopt SOL_SOCKET : %d error: %d\n", gdb_if_serv,
-			strerror(errno));
-#endif
-			close(gdb_if_serv);
-			continue;
-		}
-		if (setsockopt(gdb_if_serv, IPPROTO_TCP, TCP_NODELAY, (void*)&opt, sizeof(opt)) == -1) {
-#if defined(_WIN32) || defined(__CYGWIN__)
-			DEBUG_WARN("error setsockopt IPPROTO_TCP : %d error: %d\n", gdb_if_serv,
-			WSAGetLastError());
-#else
-			DEBUG_WARN("error setsockopt IPPROTO_TCP : %d error: %d\n", gdb_if_serv,
-			strerror(errno));
-#endif
-			close(gdb_if_serv);
-			continue;
-		}
-		if (bind(gdb_if_serv, (void*)&addr, sizeof(addr)) == -1) {
-#if defined(_WIN32) || defined(__CYGWIN__)
-			DEBUG_WARN("error when binding socket: %d error: %d\n", gdb_if_serv,
-			WSAGetLastError());
-#else
-			DEBUG_WARN("error when binding socket: %d error: %d\n", gdb_if_serv,
-			strerror(errno));
-#endif
-			close(gdb_if_serv);
-			continue;
-		}
-		if (listen(gdb_if_serv, 1) == -1) {
-			DEBUG_WARN("listen closed %d\n",gdb_if_serv);
-			close(gdb_if_serv);
-			continue;
-		}
-		break;
-	} while(1);
-	DEBUG_WARN("Listening on TCP: %4d\n", port);
+	DEBUG_WARN("Listening on TCP: %4d\n", create_socket(DEFAULT_PORT - 1, & gdb_if_serv));
 
 	return 0;
 }
@@ -126,63 +231,9 @@ unsigned char gdb_if_getchar(void)
 {
 	unsigned char ret;
 	int i = 0;
-#if defined(_WIN32) || defined(__CYGWIN__)
-	int iResult;
-	unsigned long opt;
-#else
-	int flags;
-#endif
 	while(i <= 0) {
-		if(gdb_if_conn <= 0) {
-#if defined(_WIN32) || defined(__CYGWIN__)
-			opt = 1;
-			iResult = ioctlsocket(gdb_if_serv, FIONBIO, &opt);
-			if (iResult != NO_ERROR) {
-				DEBUG_WARN("ioctlsocket failed with error: %ld\n", iResult);
-			}
-#else
-			flags = fcntl(gdb_if_serv, F_GETFL);
-			fcntl(gdb_if_serv, F_SETFL, flags | O_NONBLOCK);
-#endif
-			while(1) {
-				gdb_if_conn = accept(gdb_if_serv, NULL, NULL);
-				if (gdb_if_conn == -1) {
-#if defined(_WIN32) || defined(__CYGWIN__)
-					if (WSAGetLastError() == WSAEWOULDBLOCK) {
-#else
-					if (errno == EWOULDBLOCK) {
-#endif
-						SET_IDLE_STATE(1);
-						platform_delay(100);
-					} else {
-#if defined(_WIN32) || defined(__CYGWIN__)
-						DEBUG_WARN("error when accepting connection: %d",
-								   WSAGetLastError());
-#else
-						DEBUG_WARN("error when accepting connection: %s",
-								   strerror(errno));
-#endif
-						exit(1);
-					}
-				} else {
-#if defined(_WIN32) || defined(__CYGWIN__)
-					opt = 0;
-					ioctlsocket(gdb_if_serv, FIONBIO, &opt);
-#else
-					fcntl(gdb_if_serv, F_SETFL, flags);
-#endif
-					break;
-				}
-			}
-			DEBUG_INFO("Got connection\n");
-#if defined(_WIN32) || defined(__CYGWIN__)
-			opt = 0;
-			ioctlsocket(gdb_if_conn, FIONBIO, &opt);
-#else
-			flags = fcntl(gdb_if_conn, F_GETFL);
-			fcntl(gdb_if_conn, F_SETFL, flags & ~O_NONBLOCK);
-#endif
-		}
+		if(gdb_if_conn <= 0)
+			gdb_if_conn = socket_accept(gdb_if_serv);
 		i = recv(gdb_if_conn, (void*)&ret, 1, 0);
 		if(i <= 0) {
 			gdb_if_conn = -1;
